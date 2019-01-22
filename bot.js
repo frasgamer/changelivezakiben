e9ba576c7db9ccb23153fa996310eeae62f28e10
nasrfhqgjh <44925264+frasgamer@users.noreply.github.com>
2019-01-22 21:16:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,28 +16,28 @@ client.on('ready', () => {
 
 
 
-const adminprefix = "Z";//FRAS
-const devs = ['307666420743798784','313377631783485440'];//FRAS
-client.on('message', message => {//FRAS
-  var argresult = message.content.split(` `).slice(1).join(' ');//FRAS
-    if (!devs.includes(message.author.id)) return;//FRAS
+const adminprefix = "Z";
+const devs = ['307666420743798784','313377631783485440'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
     
-if (message.content.startsWith(adminprefix + 'play')) {//fras
-  client.user.setGame(argresult);//fras
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)//fras
-} else //fras
+if (message.content.startsWith(adminprefix + 'play')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
   if (message.content.startsWith(adminprefix + 'name')) {//fras
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'avatar')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else     
-if (message.content.startsWith(adminprefix + 'streaming')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");//wennnn
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+client.user.setUsername(argresult).then//fras
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)//fras
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");//fras
+} else//fras
+  if (message.content.startsWith(adminprefix + 'avatar')) {/fras
+client.user.setAvatar(argresult);//fras
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);//fras
+      } else     //fras
+if (message.content.startsWith(adminprefix + 'streaming')) {//fras
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");//Fras
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)//fras
 }
 });
 
